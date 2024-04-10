170c9d45944005c66b530929943ba1ac01dbcff1
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-10 09:54:47 -0300

    Add Console Write ClientInfo

--- Controllers/BasicAuth.cs
+++ Controllers/BasicAuth.cs
@@ -1,3 +1,5 @@
+using BasicAuthHandler.Global;
+using BasicAuthHandler.Models;
 using BasicAuthHandler.Properties;
 using Microsoft.AspNetCore.Mvc;
 using RoundTheCode.BasicAuthentication.Authentication.Basic.Attributes;
@@ -13,6 +15,12 @@ namespace BasicAuthHandler.Controllers
         [BasicAuthorization]
         public ActionResult Get()
         {
+            ClientAcceptModel clientAcceptModel = 
+                GlobalVariables.ClientsAccept[Request.HttpContext.Connection.RemoteIpAddress!];
+
+            Console.WriteLine($"Username: {clientAcceptModel.Username} Key: {clientAcceptModel.Key}");
+
+            //The client will only reach the Controller if the authorization is successful.
             return new ContentResult
             {
                 StatusCode = (int)HttpStatusCode.OK,
