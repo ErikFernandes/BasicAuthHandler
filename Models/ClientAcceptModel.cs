8d1825ed1799a06b01aed333ff74f27f51e6f1b5
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-10 09:54:05 -0300

    Add ClientAcceptModel

---
+++ Models/ClientAcceptModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace BasicAuthHandler.Models
+{
+    public class ClientAcceptModel
+    {
+        public IPAddress? IpAddress { get; set; }
+        public int HitsAccept { get; set; }
+        public DateTime LastAccessDate { get; set; } = DateTime.Now;
+        public string Username { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+        public bool IsLocalhost => IpAddress!.Equals(IPAddress.IPv6Loopback);
+
+    }
+}
