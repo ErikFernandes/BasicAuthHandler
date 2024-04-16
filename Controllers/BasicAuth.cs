89d9cbb973882323c0c85f5d62ea321389ea0e57
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-16 15:51:21 -0300

    Add Method POST BasicAuth

--- Controllers/BasicAuth.cs
+++ Controllers/BasicAuth.cs
@@ -3,6 +3,7 @@ using BasicAuthHandler.Models;
 using Microsoft.AspNetCore.Mvc;
 using RoundTheCode.BasicAuthentication.Authentication.Basic.Attributes;
 using System.Net;
+using System.Text.Json;
 
 namespace BasicAuthHandler.Controllers
 {
@@ -11,13 +12,14 @@ namespace BasicAuthHandler.Controllers
     [Route("BasicAuth")]
     public class BasicAuth : ControllerBase
     {
+        
         [HttpGet]
         public ActionResult Get()
         {
-            ClientAcceptModel clientAcceptModel = 
+            ClientAcceptModel clientAcceptModel =
                 GlobalVariables.ClientsAccept[Request.HttpContext.Connection.RemoteIpAddress!];
 
-            Console.WriteLine($"Username: {clientAcceptModel.Username} Key: {clientAcceptModel.Key}");
+            Console.WriteLine($"GET: Username: {clientAcceptModel.Username} Key: {clientAcceptModel.Key}");
 
             //The client will only reach the Controller if the authorization is successful.
             return new ContentResult
@@ -26,6 +28,27 @@ namespace BasicAuthHandler.Controllers
                 ContentType = "text",
                 Content = "Access Granted!"
             };
+        }
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody] JsonElement json)
+        {
+            ClientAcceptModel clientAcceptModel =
+                GlobalVariables.ClientsAccept[Request.HttpContext.Connection.RemoteIpAddress!];
+
+            Console.WriteLine($"POST: Username: {clientAcceptModel.Username} Key: {clientAcceptModel.Key}");
+            Console.WriteLine($"BODY: {json}");
+
+            //The client will only reach the Controller if the authorization is successful.
+            return new ContentResult
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                ContentType = "text",
+                Content = "Access Granted!"
+            };
+
+
         }
     }
 }
